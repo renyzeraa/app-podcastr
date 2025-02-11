b61717879063076ab6c404017469960225cd4d38
Renan Silva <renansilvaytB@gmail.com>
2025-02-10 21:53:50 -0300

    feat: fetch api episodes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { Player } from "@/components/player";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("http://localhost:3333/episodes", {
+    cache: "force-cache",
+    next: { revalidate: 28800 }
+  });
+  const episodes = await response.json();
+  console.log(episodes);
   return (
     <main>
       <Player />
