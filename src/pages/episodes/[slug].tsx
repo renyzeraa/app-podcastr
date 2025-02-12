042301ba060b3876d20ca40d71b58dbdb0c2c8c0
Renan Silva <renansilvaytB@gmail.com>
2025-02-12 19:13:23 -0300

    fix: type for MyApp

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -115,6 +115,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
     props: {
       episode,
     },
-    revalidate: 60 * 60 * 24, // 24 hours
+    revalidate: 60 * 60 * 8,
   }
 }
